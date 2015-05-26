cc3505ddb03f4fd7962e3b3ef534634709246b0e
NexPB <nex.powerbot@hotmail.com>
2015-05-26 16:58:13 +0200

    use __call for the static api

--- src/Game/LeagueStatic.php
+++ src/Game/LeagueStatic.php
@@ -2,39 +2,58 @@
 
 use LeagueData\Core\Api;
 
+/**
+ * Class LeagueStatic
+ * @package LeagueData\Game
+ *
+ * @method champion($id = 0, $data = 'default');
+ * @method item($id = 0, $data = 'default');
+ * @method spell($id = 0, $data = 'default');
+ * @method mastery($id = 0, $data = 'default');
+ * @method rune($id = 0, $data = 'default');
+ */
 class LeagueStatic extends Api {
 
     protected $version = 'v1.2';
     protected $base_url = 'https://global.api.pvp.net/api/lol/static-data/%s/%s/%sapi_key=%s';
 
-    /**
-     * Returns Champion(s) data.
-     *
-     * @param int $champ_id
-     * @param string $champ_data
-     * @return mixed|null
-     * @throws \HttpException
-     */
-    public function champion($champ_id = 0, $champ_data = '') {
-        if (is_numeric($champ_id) && $champ_id > 0)
-            return $this->request('champion/' . $champ_id . '?champData=' . (empty($champ_data) ? 'image,info,tags,stats,spells,skins,passive' : $champ_data) . '&');
+    public $data = [
+        'champion' => [
+            'query' => 'champion',
+            'query_data' => 'champData',
+            'default_data' => 'image,info,tags,stats,spells,skins,passive'
+        ],
+        'item' => [
+            'query' => 'item',
+            'query_data' => 'itemListData',
+            'query_single_data' => 'itemData',
+            'default_data' => 'all'
+        ],
+        'spell' => [
+            'query' => 'summoner-spell',
+            'query_data' => 'spellData',
+            'default_data' => 'all'
+        ],
+        'mastery' => [
+            'query' => 'mastery',
+            'query_data' => 'masteryList',
+            'default_data' => 'all'
+        ],
+        'rune' => [
+            'query' => 'rune',
+            'query_data' => 'runeListData',
+            'default_data' => 'all'
+        ]
+    ];
 
-        return $this->request('champion?champData=' . (empty($champ_data) ? 'image,info,tags,stats,spells,skins,passive' : $champ_data) . '&');
-    }
+    public function __call($name, $args) {
+        if (!isset($this->data[$name]))
+            throw new \Exception("No such method exists!");
 
-    /**
-     * Returns item(s) data
-     *
-     * @param int $item_id
-     * @param string $item_data
-     * @return mixed|null
-     * @throws \HttpException
-     */
-    public function item($item_id = 0, $item_data = 'all') {
-        if (is_numeric($item_id) && $item_id > 0)
-            return $this->request('item/' . $item_id . '?itemData=' . $item_data . '&');
+        if (isset($args[0]) && is_numeric($args[0]))
+            return $this->request($this->data[$name]['query'] . '/' . $args[0] . '?' . (isset($this->data[$name]['query_single_data']) ? $this->data[$name]['query_single_data'] : $this->data[$name]['query_data']) . '=' . (isset($args[1]) ? $args[1] : $this->data[$name]['default_data']) . '&');
 
-        return $this->request('item?itemListData=' . $item_data . '&');
+        return $this->request($this->data[$name]['query'] . '?' . $this->data[$name]['query_data'] . '=' . (isset($args[1]) ? $args[1] : $this->data[$name]['default_data']) . '&');
     }
 
     protected function url($query) {
