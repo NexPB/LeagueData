d7e652a60f7c735aea18e0659ec77c77ae95872a
NexPB <nex.powerbot@hotmail.com>
2015-06-26 13:51:54 +0200

    Add esports api support

--- src/EsportsData.php
+++ src/EsportsData.php
@@ -6,14 +6,60 @@ class EsportsData extends Api {
 
     protected $base_url = 'http://%s.lolesports.com:80/api/%s';
 
+    /**
+     * Instantiate a new EsportsData
+     */
     public function __construct() {
         $this->region = 'na';
     }
 
-    public function getTournaments() {
+    /**
+     * Returns an array with tournament data.
+     *
+     * @param int $id (default = 0 and will return all tournaments in that case)
+     * @return mixed
+     * @throws Core\Exception\HttpException503
+     * @throws Core\Exception\HttpExceptionUnknown
+     */
+    public function getTournament($id = 0) {
+        if ($id > 0)
+            return $this->request('tournament/' . $id . '.json');
+
         return $this->request('tournament.json?published=1');
     }
 
+    /**
+     * Returns an array with team data
+     *
+     * @param $id required
+     * @param bool $expand show all player info.
+     * @return mixed
+     * @throws Core\Exception\HttpException503
+     * @throws Core\Exception\HttpExceptionUnknown
+     */
+    public function getTeam($id, $expand = false) {
+        return $this->request('team/' . $id . '.json' . ($expand ? '?expandPlayers=1' : ''));
+    }
+
+    /**
+     * Returns an array with detailed player info.
+     *
+     * @param $id required
+     * @return mixed
+     * @throws Core\Exception\HttpException503
+     * @throws Core\Exception\HttpExceptionUnknown
+     */
+    public function getPlayer($id) {
+        return $this->request('player/' . $id . '.json');
+    }
+
+    /**
+     * Overrides parent class url method.
+     *
+     * @param $query
+     * @param bool $append_api_key
+     * @return string
+     */
     public function url($query, $append_api_key = false) {
         return sprintf($this->base_url, $this->region, $query);
     }
